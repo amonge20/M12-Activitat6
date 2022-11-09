14397d147e21dcce2e4967713b6239caccbaaa31
amonge20 <amonge20@jaumebalmes.net>
2022-11-09 17:38:07 +0100

    Començant activitat6

---
+++ practica6/src/test/java/com/example/jaumebalmes/net/Practica6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaumebalmes.net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
