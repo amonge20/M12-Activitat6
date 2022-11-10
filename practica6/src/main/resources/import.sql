8349492053b431090126ca40a061c6920d0a5180
amonge20 <amonge20@jaumebalmes.net>
2022-11-10 17:06:48 +0100

    Creació de Course feta i BBDD feta

---
+++ practica6/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into import (id, title) values (1, 'DAW1');
+insert into import (id, title) values (2, 'DAW2');
+insert into import (id, title) values (3, 'DAM1');
+insert into import (id, title) values (4, 'DAM2');
+insert into import (id, title) values (5, 'ASIX1');
+insert into import (id, title) values (6, 'ASIX2');
